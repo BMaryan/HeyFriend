3f6cac83fc0a53618f74bba441077ad037f6b38e
BMaryan <burbanmaryan02@gmail.com>
2021-09-01 14:41:45 +0300

    Refactor folder Stories

--- src/components/common/Stories/Story/Story.jsx
+++ src/components/common/Stories/Story/Story.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import styles from "../Stories.module.css";
+import styles from "./Story.module.css";
 import defaultAvatar from "../../../../assets/images/DefaultAvatar.png";
 
 const Story = props => {
 	return (
-		<div className={styles.wrapper_story}>
-			<div className={styles.story}>
+		<div className={styles.story}>
+			<div className={styles.wrapper_content}>
 				<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuCd-m7iQF1pBft6SEEeMl7zQTJBPQCA8vSg&usqp=CAU' alt='' />
 			</div>
+
 			<div className={styles.wrapper_footer}>
 				{/* <div className={styles.wrapper_picture}>
 					<img className={styles.picture} src={props.profile && props.profile.img ? props.profile.img : defaultAvatar} alt='' />
 				</div> */}
-				<div className={styles.login}>{props.profile ? props.profile.surname + " " + props.profile.name : <></>}</div>
+				<div className={styles.fullName}>{props.profile ? props.profile.surname + " " + props.profile.name : <></>}</div>
 			</div>
 		</div>
 	);
