30f0750e7c8a109e4c25e8a989619d88e8cfbc3c
Burban Maryan <burbanmaryan02@gmail.com>
2022-04-02 12:48:44 +0300

    renamed social network

--- src/redux/auth-reducer.jsx
+++ src/redux/auth-reducer.jsx
@@ -1,62 +1,62 @@
-let SET_USER_SIGN_IN = "social_network/auth/SET_USER_SIGN_IN";
-let SET_USER_SIGN_UP = "social_network/auth/SET_USER_SIGN_UP";
-let GET_DEFAULT_ACCOUNT = "social_network/auth/GET_DEFAULT_ACCOUNT";
+let SET_USER_SIGN_IN = "heyfriend/auth/SET_USER_SIGN_IN";
+let SET_USER_SIGN_UP = "heyfriend/auth/SET_USER_SIGN_UP";
+let GET_DEFAULT_ACCOUNT = "heyfriend/auth/GET_DEFAULT_ACCOUNT";
 
 let initialState = {
-	userSignIn: {
-		phone_or_email: null,
-		password: null,
-		rememberMe: null,
-	},
-	userSignUp: {
-		id: null,
-		name: null,
-		surname: null,
-		phone_or_email: null,
-		password: null,
-	},
-	defaultAccount: null,
+  userSignIn: {
+    phone_or_email: null,
+    password: null,
+    rememberMe: null,
+  },
+  userSignUp: {
+    id: null,
+    name: null,
+    surname: null,
+    phone_or_email: null,
+    password: null,
+  },
+  defaultAccount: null,
 };
 
 const AuthReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case SET_USER_SIGN_IN: {
-			return {
-				...state,
-				userSignIn: { ...action.data },
-			};
-		}
-		case SET_USER_SIGN_UP: {
-			return {
-				...state,
-				userSignUp: { ...action.data },
-			};
-		}
-		case GET_DEFAULT_ACCOUNT: {
-			return {
-				...state,
-				defaultAccount: action.account ? action.account : null,
-			};
-		}
-		default: {
-			return state;
-		}
-	}
+  switch (action.type) {
+    case SET_USER_SIGN_IN: {
+      return {
+        ...state,
+        userSignIn: { ...action.data },
+      };
+    }
+    case SET_USER_SIGN_UP: {
+      return {
+        ...state,
+        userSignUp: { ...action.data },
+      };
+    }
+    case GET_DEFAULT_ACCOUNT: {
+      return {
+        ...state,
+        defaultAccount: action.account ? action.account : null,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
 export const setUserSignIn = ({ phone_or_email, password, rememberMe = false }) => ({
-	type: SET_USER_SIGN_IN,
-	data: { phone_or_email, password, rememberMe },
+  type: SET_USER_SIGN_IN,
+  data: { phone_or_email, password, rememberMe },
 });
 
 export const setUserSignUp = ({ name, surname, phone_or_email, password }) => ({
-	type: SET_USER_SIGN_UP,
-	data: { name, surname, phone_or_email, password },
+  type: SET_USER_SIGN_UP,
+  data: { name, surname, phone_or_email, password },
 });
 
-export const getDefaultAccount = account => ({
-	type: GET_DEFAULT_ACCOUNT,
-	account,
+export const getDefaultAccount = (account) => ({
+  type: GET_DEFAULT_ACCOUNT,
+  account,
 });
 
 export default AuthReducer;
--- src/redux/chat-reducer.jsx
+++ src/redux/chat-reducer.jsx
@@ -1,115 +1,113 @@
-let ADD_MESSAGE = "social_network/chatPage/ADD_MESSAGE";
-let ADD_CHAT = "social_network/chatPage/ADD_CHAT";
+let ADD_MESSAGE = "heyfriend/chatPage/ADD_MESSAGE";
+let ADD_CHAT = "heyfriend/chatPage/ADD_CHAT";
 
 let initialState = {
-	chats: [
-		// {
-		// 	id: 1,
-		// 	messages: [
-		// 		{
-		// 			id: 2,
-		// 			userId: 3,
-		// 			message: "HEllo",
-		// 		},
-		// 		{
-		// 			id: 1,
-		// 			userId: 3,
-		// 			message: "How are you",
-		// 		},
-		// 		{
-		// 			id: 1,
-		// 			userId: 1,
-		// 			message: "I'm good",
-		// 		},
-		// 	],
-		// },
-		// {
-		// 	id: 2,
-		// 	messages: [
-		// 		{
-		// 			id: 2,
-		// 			userId: 2,
-		// 			message: "HEllo",
-		// 		},
-		// 		{
-		// 			id: 2,
-		// 			userId: 2,
-		// 			message: "How are you",
-		// 		},
-		// 		{
-		// 			id: 2,
-		// 			userId: 1,
-		// 			message: "I'm good",
-		// 		},
-		// 	],
-		// },
-		// {
-		// 	id: 3,
-		// 	messages: [
-		// 		{
-		// 			id: 3,
-		// 			userId: 2,
-		// 			message: "HEllo",
-		// 		},
-		// 		{
-		// 			id: 3,
-		// 			userId: 2,
-		// 			message: "How are you",
-		// 		},
-		// 		{
-		// 			id: 3,
-		// 			userId: 3,
-		// 			message: "I'm good",
-		// 		},
-		// 	],
-		// },
-	],
+  chats: [
+    // {
+    // 	id: 1,
+    // 	messages: [
+    // 		{
+    // 			id: 2,
+    // 			userId: 3,
+    // 			message: "HEllo",
+    // 		},
+    // 		{
+    // 			id: 1,
+    // 			userId: 3,
+    // 			message: "How are you",
+    // 		},
+    // 		{
+    // 			id: 1,
+    // 			userId: 1,
+    // 			message: "I'm good",
+    // 		},
+    // 	],
+    // },
+    // {
+    // 	id: 2,
+    // 	messages: [
+    // 		{
+    // 			id: 2,
+    // 			userId: 2,
+    // 			message: "HEllo",
+    // 		},
+    // 		{
+    // 			id: 2,
+    // 			userId: 2,
+    // 			message: "How are you",
+    // 		},
+    // 		{
+    // 			id: 2,
+    // 			userId: 1,
+    // 			message: "I'm good",
+    // 		},
+    // 	],
+    // },
+    // {
+    // 	id: 3,
+    // 	messages: [
+    // 		{
+    // 			id: 3,
+    // 			userId: 2,
+    // 			message: "HEllo",
+    // 		},
+    // 		{
+    // 			id: 3,
+    // 			userId: 2,
+    // 			message: "How are you",
+    // 		},
+    // 		{
+    // 			id: 3,
+    // 			userId: 3,
+    // 			message: "I'm good",
+    // 		},
+    // 	],
+    // },
+  ],
 };
 
 const ChatReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_CHAT: {
-			let newChat = {
-				id: action.id,
-				messages: [],
-			};
+  switch (action.type) {
+    case ADD_CHAT: {
+      let newChat = {
+        id: action.id,
+        messages: [],
+      };
 
-			return {
-				...state,
-				// chats: state.chats && state.chats.filter(chat => (chat.id === action.id ? [...state.chats] : [...state.chats, { ...newChat }])),
-				chats: [...state.chats, { ...newChat }],
-			};
-		}
-		case ADD_MESSAGE: {
-			let newMessage = {
-				id: state.chats.map(chat => (chat.id === action.id ? chat.messages.length : undefined)),
-				userId: action.userId,
-				message: action.message,
-			};
+      return {
+        ...state,
+        // chats: state.chats && state.chats.filter(chat => (chat.id === action.id ? [...state.chats] : [...state.chats, { ...newChat }])),
+        chats: [...state.chats, { ...newChat }],
+      };
+    }
+    case ADD_MESSAGE: {
+      let newMessage = {
+        id: state.chats.map((chat) => (chat.id === action.id ? chat.messages.length : undefined)),
+        userId: action.userId,
+        message: action.message,
+      };
 
-			return {
-				...state,
-				chats: state.chats.map(chat =>
-					chat.id === action.id ? { ...chat, messages: [...chat.messages, newMessage] } : { ...chat, messages: [...chat.messages] }
-				),
-			};
-		}
-		default: {
-			return state;
-		}
-	}
+      return {
+        ...state,
+        chats: state.chats.map((chat) => (chat.id === action.id ? { ...chat, messages: [...chat.messages, newMessage] } : { ...chat, messages: [...chat.messages] })),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
-export const addChat = id => ({
-	type: ADD_CHAT,
-	id,
+export const addChat = (id) => ({
+  type: ADD_CHAT,
+  id,
 });
 
 export const addMessage = (id, userId, message) => ({
-	type: ADD_MESSAGE,
-	id,
-	userId,
-	message,
+  type: ADD_MESSAGE,
+  id,
+  userId,
+  message,
 });
 
 export default ChatReducer;
