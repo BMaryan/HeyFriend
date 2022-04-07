75a744365794e8b4064a82a2f3c49019b521d168
Burban Maryan <burbanmaryan02@gmail.com>
2022-04-08 00:46:57 +0300

    added signIn thunk and created some cases

--- src/redux/auth-reducer.jsx
+++ src/redux/auth-reducer.jsx
@@ -1,6 +1,10 @@
+import { authFb } from "../functionsFb/functionsFb";
+
 let SET_USER_SIGN_IN = "heyfriend/auth/SET_USER_SIGN_IN";
 let SET_USER_SIGN_UP = "heyfriend/auth/SET_USER_SIGN_UP";
 let GET_DEFAULT_ACCOUNT = "heyfriend/auth/GET_DEFAULT_ACCOUNT";
+let LOGIN_SUCCESS = "heyfriend/auth/LOGIN_SUCCESS";
+let LOGIN_ERROR = "heyfriend/auth/LOGIN_ERROR";
 
 let initialState = {
   userSignIn: {
@@ -15,6 +19,7 @@ let initialState = {
     phone_or_email: null,
     password: null,
   },
+  authError: null,
   defaultAccount: null,
 };
 
@@ -29,13 +34,25 @@ const AuthReducer = (state = initialState, action) => {
     case SET_USER_SIGN_UP: {
       return {
         ...state,
-        userSignUp: { ...action.data },
+        userSignUp: { ...action.credentials },
       };
     }
     case GET_DEFAULT_ACCOUNT: {
       return {
         ...state,
         defaultAccount: action.account ? action.account : null,
+      };
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        authError: null,
+      };
+    }
+    case LOGIN_ERROR: {
+      return {
+        ...state,
+        authError: action.error,
       };
     }
     default: {
@@ -49,14 +66,48 @@ export const setUserSignIn = ({ phone_or_email, password, rememberMe = false }) 
   data: { phone_or_email, password, rememberMe },
 });
 
-export const setUserSignUp = ({ name, surname, phone_or_email, password }) => ({
+export const setUserSignUp = (credentials) => ({
   type: SET_USER_SIGN_UP,
-  data: { name, surname, phone_or_email, password },
+  credentials: { ...credentials },
 });
 
 export const getDefaultAccount = (account) => ({
   type: GET_DEFAULT_ACCOUNT,
   account,
 });
+
+export const loginSuccess = () => ({
+  type: LOGIN_SUCCESS,
+});
+
+export const loginError = (error) => ({
+  type: LOGIN_ERROR,
+  error,
+});
+
+// thunks
+export const signUp = (credentials) => async (dispatch) => {
+  try {
+    await authFb.signUp({ email: credentials.phone_or_email, password: credentials.password });
+
+    dispatch(loginSuccess());
+  } catch (error) {
+    dispatch(loginError(error));
+  }
+};
+
+export const signIn = (credentials) => async (dispatch) => {
+  try {
+    await authFb.signIn({ email: credentials.phone_or_email, password: credentials.password });
+
+    dispatch(loginSuccess());
+
+    alert("Welcome");
+  } catch (error) {
+    dispatch(loginError(error));
+
+    alert("Fatal");
+  }
+};
 
 export default AuthReducer;
