0e8b88350cb6e775cbafdab7e97c765e5e123f36
Burban Maryan <burbanmaryan02@gmail.com>
2022-04-15 13:00:56 +0300

    renamed thunkMiddleware

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,18 +1,18 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
+import thunk from "redux-thunk";
 import AuthReducer from "./auth-reducer";
 import { reducer as formReducer } from "redux-form";
 import ProfileReducer from "./profile-reducer";
 import ChatReducer from "./chat-reducer";
 
 let reducers = combineReducers({
-	profilePage: ProfileReducer,
-	chatPage: ChatReducer,
-	form: formReducer,
-	auth: AuthReducer,
+  profilePage: ProfileReducer,
+  chatPage: ChatReducer,
+  form: formReducer,
+  auth: AuthReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+let store = createStore(reducers, applyMiddleware(thunk));
 
 window.store = store;
 
