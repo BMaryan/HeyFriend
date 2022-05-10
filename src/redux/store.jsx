c7419355a2e86328a53500274a3cbb35ef88f1d3
Burban Maryan <burbanmaryan02@gmail.com>
2022-05-10 20:15:46 +0300

    added post reducer to combineReducers

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -4,10 +4,12 @@ import AuthReducer from "./auth-reducer";
 import { reducer as formReducer } from "redux-form";
 import ProfileReducer from "./profile-reducer";
 import ChatReducer from "./chat-reducer";
+import PostPage from "./post-reducer";
 
 let reducers = combineReducers({
   profilePage: ProfileReducer,
   chatPage: ChatReducer,
+  postPage: PostPage,
   form: formReducer,
   auth: AuthReducer,
 });
