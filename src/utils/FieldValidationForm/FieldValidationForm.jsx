b7689c5cb6eca2a34003599721f488d5ac529143
Burban Maryan <burbanmaryan02@gmail.com>
2022-05-10 20:24:44 +0300

    added new innovations and field reactions

--- src/utils/FieldValidationForm/FieldValidationForm.jsx
+++ src/utils/FieldValidationForm/FieldValidationForm.jsx
@@ -1,15 +1,8 @@
-import styles from "./FieldValidationForm.module.scss";
-
 export const required = (value) => {
   if (value) {
     return undefined;
   } else {
-<<<<<<< HEAD
     return "This field is required";
-    // return <div className={styles.required}>This field is required</div>;
-=======
-    return <div className={styles.required}>This field is required</div>;
->>>>>>> 829743376670bcf6bd688d0d118905c801d65516
   }
 };
 
@@ -18,11 +11,7 @@ export const maxLengthCreator = (maxLength) => (value) => value && value.length 
 export const validateAuthorizationUserCreator = (accounts, userSignIn) => {
   let foundAccount;
   if (accounts) {
-<<<<<<< HEAD
-    foundAccount = accounts && accounts.find((account) => (account.email === userSignIn.email && account.password === userSignIn.password) || (account.profile && account.profile.email === userSignIn.email && account.profile.password === userSignIn.password));
-=======
-    foundAccount = accounts && accounts.find((account) => (account.phone_or_email === userSignIn.phone_or_email && account.password === userSignIn.password) || (account.profile && account.profile.phone_or_email === userSignIn.phone_or_email && account.profile.password === userSignIn.password));
->>>>>>> 829743376670bcf6bd688d0d118905c801d65516
+    foundAccount = accounts && accounts.find((account) => (account.email === userSignIn.email && account.password === userSignIn.password) || (account?.email === userSignIn.email && account?.password === userSignIn.password));
 
     if (foundAccount) {
       return undefined;
@@ -35,11 +24,7 @@ export const validateAuthorizationUserCreator = (accounts, userSignIn) => {
 export const validateFindTheSameUserCreator = (accounts, userSignUp) => {
   let foundUser;
   if (accounts) {
-<<<<<<< HEAD
-    foundUser = accounts && accounts.find((account) => (account.profile.email === userSignUp.email ? account : undefined));
-=======
-    foundUser = accounts && accounts.find((account) => (account.profile.phone_or_email === userSignUp.phone_or_email ? account : undefined));
->>>>>>> 829743376670bcf6bd688d0d118905c801d65516
+    foundUser = accounts && accounts.find((account) => (account.email === userSignUp.email ? account : undefined));
 
     if (!foundUser) {
       return undefined;
@@ -57,23 +42,17 @@ export const validateFirstAndLastNameCreator = (nameField) => (value) => {
   }
 };
 
-<<<<<<< HEAD
 export const validateEmail = (value) => {
   if (value && !/^[\w-.]+@[a-zA-Z]+\.[a-zA-Z]+$/i.test(value)) {
-    return "The email you entered isn’t connected to an account.";
-=======
-export const validatePhoneNumberAndEmail = (value) => {
-  if (value && !/^([0-9][0-9]{9})$/i.test(value) && value && !/^[\w-.]+@[a-zA-Z]+\.[a-zA-Z]+$/i.test(value)) {
-    return "The email or mobile number you entered isn’t connected to an account.";
->>>>>>> 829743376670bcf6bd688d0d118905c801d65516
+    return value.includes("@") ? `The email you entered isn’t connected to an account.` : `You missed the @ symbol. Please enter your email correctly.`;
   } else {
     return undefined;
   }
 };
 
 export const validatePassword = (value) => {
-  if (value && !/^[\w.]{8,}$/i.test(value)) {
-    return "Sorry, your password was incorrect. Please double-check your password.";
+  if (value && !/^[\w.]{8,15}$/i.test(value)) {
+    return `Sorry, your password is incorrect. ${value.length > 8 ? `You have entered too many characters: ${value.length}. Maximum characters must be 15` : `You didn't enter enough characters: ${value.length}. Minimum characters must be 8`}`;
   } else {
     return undefined;
   }
