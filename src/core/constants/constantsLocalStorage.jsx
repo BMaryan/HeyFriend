cf7d86ade400c529fddfcef4358c69dbafbb1298
Burban Maryan <burbanmaryan02@gmail.com>
2022-07-09 19:23:57 +0300

    migrated from jsx to tsx

--- src/core/constants/constantsLocalStorage.jsx
+++
@@ -1,2 +0,0 @@
-export const accounts = "accounts";
-export const account = "account";
--- src/defaultAccounts/defaultAccounts.js
+++
@@ -1,7 +0,0 @@
-import { account1 } from "./Account1/Account1";
-import { account2 } from "./Account2/Account2";
-import { account3 } from "./Account3/Account3";
-
-const defaultAccounts = [account1, account2, account3];
-
-export default defaultAccounts;
