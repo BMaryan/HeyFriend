6bb169ec02e26855c29c7b6eef7d30a733e88b6c
Burban Maryan <burbanmaryan02@gmail.com>
2022-07-31 20:42:27 +0300

    test

--- src/core/constants/constantsPost.jsx
+++ src/core/constants/constantsPost.jsx
@@ -1,4 +1,4 @@
 // constants to check the display of the post
-export let defaultPostConstant = "DEFAULT_POST";
-export let modalPostConstant = "MODAL_POST_CONSTANT";
-export let onlyBodyPostConstant = "ONLY_BODY_POST_CONSTANT";
+export const defaultPostConstant = "DEFAULT_POST";
+export const modalPostConstant = "MODAL_POST_CONSTANT";
+export const onlyBodyPostConstant = "ONLY_BODY_POST_CONSTANT";
