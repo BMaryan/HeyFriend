58bce90c117fd184606b88169cd1c667910bd93f
Burban Maryan <burbanmaryan02@gmail.com>
2022-08-21 12:44:01 +0300

    added new properties to type

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import defaultAvatar from "../../../assets/images/DefaultAvatar.png";
+import { AccountType } from "../../../types/types";
 import styles from "./Avatar.module.scss";
 import Avatar from "@mui/material/Avatar";
+import { Tooltip } from "@mui/material";
 
 interface AvatarPropsType {
-  avatarData: any;
+  avatarData: AccountType;
+  title?: string;
   size?: "small" | "medium" | "large" | "full";
+  variant?: "circular" | "rounded" | "square";
+  onClick?: () => void;
 }
 
 const CustomAvatar = (props: AvatarPropsType) => {
-  const size = props.size === "small" ? "10px" : props.size === "medium" ? "20px" : props.size === "large" ? "30px" : props.size === "full" ? "100%" : undefined;
-  return <Avatar sx={{ maxHeight: size, maxWidth: size }} src={props.avatarData.avatar || defaultAvatar} alt="" />;
+  const size = props.size === "small" ? "15px" : props.size === "medium" ? "30px" : props.size === "large" ? "45px" : props.size === "full" ? "100%" : undefined;
+  return (
+    <Tooltip title={props.title ? props.title : props.avatarData.surname + " " + props.avatarData.name}>
+      <Avatar sx={{ maxHeight: size, maxWidth: size, cursor: props.onClick && "pointer" }} src={props.avatarData.avatar || defaultAvatar} alt="" variant={props.variant} onClick={props.onClick} />
+    </Tooltip>
+  );
 };
 
 export default CustomAvatar;
