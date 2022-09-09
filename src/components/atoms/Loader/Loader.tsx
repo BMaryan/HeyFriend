0b32c72f71ba08408e1ce35244cbf3b1f00159f8
Burban Maryan <burbanmaryan02@gmail.com>
2022-09-09 13:44:27 +0300

    created loader

---
+++ src/components/atoms/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Loader.module.scss";
+import { CircularProgress } from "@mui/material";
+
+interface LoaderPropsType {}
+
+const Loader = (props: LoaderPropsType) => {
+  return (
+    <div className="gl_wrapper_loading">
+      <CircularProgress className="loading" />
+    </div>
+  );
+};
+
+export default Loader;
