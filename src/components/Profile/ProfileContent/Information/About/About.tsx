f03e67c1c5b7eca61cdae6281916b4b50a0a9e2c
Burban Maryan <burbanmaryan02@gmail.com>
2022-10-24 15:36:25 +0300

    added default content

--- src/components/Profile/ProfileContent/Information/About/About.tsx
+++ src/components/Profile/ProfileContent/Information/About/About.tsx
@@ -1,23 +1,39 @@
 import React from "react";
-import styles from "./About.module.scss";
+import { ProfileDataIllustration } from "../../../../../assets/illustrations/ProfileDataIllustration";
+import { heyFriendStyleConstant } from "../../../../../core/constants/constantsStyles";
 import { AccountType, FirebaseType } from "../../../../../types/types";
+import styles from "./About.module.scss";
 
 interface AboutPropsType {
   currentAccount: FirebaseType<AccountType> | undefined;
 }
 
 const About = (props: AboutPropsType) => {
+  const accountData = props?.currentAccount?.data();
+
   return (
     <div>
-      <div className={styles.wrapper_item}>
-        <div className={styles.title}>About me</div>
-        {props?.currentAccount?.data()?.aboutMe ? <div>{props?.currentAccount?.data()?.aboutMe}</div> : undefined}
-      </div>
+      {accountData?.aboutMe || accountData?.status || (accountData?.aboutMe && accountData?.status) ? (
+        <>
+          {accountData?.aboutMe && (
+            <div className={styles.wrapper_item}>
+              <div className={styles.title}>About me</div>
+              <div>{accountData?.aboutMe}</div>
+            </div>
+          )}
 
-      <div className={styles.wrapper_item}>
-        <div className={styles.title}>Status</div>
-        {props?.currentAccount?.data()?.status ? <div>{props?.currentAccount?.data()?.status}</div> : undefined}
-      </div>
+          {accountData?.status && (
+            <div className={styles.wrapper_item}>
+              <div className={styles.title}>Status</div>
+              <div>{accountData?.status}</div>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className={styles.default_content}>
+          <ProfileDataIllustration height="50%" width="50%" mainColor={heyFriendStyleConstant.first} minorColor={heyFriendStyleConstant.second} />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Profile/ProfileContent/Information/Contacts/Contacts.tsx
+++ src/components/Profile/ProfileContent/Information/Contacts/Contacts.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import styles from "./Contacts.module.scss";
+import { ProfileDataIllustration } from "../../../../../assets/illustrations/ProfileDataIllustration";
+import { heyFriendStyleConstant } from "../../../../../core/constants/constantsStyles";
 import { AccountType, FirebaseType } from "../../../../../types/types";
+import styles from "./Contacts.module.scss";
 
 interface ContactsPropsType {
   currentAccount: FirebaseType<AccountType> | undefined;
 }
 
 const Contacts = (props: ContactsPropsType) => {
-  return <div className={styles.contacts}>{props?.currentAccount?.data()?.email ? "in progress" : undefined}</div>;
+  // const accountData = props?.currentAccount?.data();
+
+  return (
+    <div className={styles.contacts}>
+      {/* {accountData?.aboutMe || accountData?.status || (accountData?.aboutMe && accountData?.status) ? (
+        <>
+          {accountData?.aboutMe && (
+            <div className={styles.wrapper_item}>
+              <div className={styles.title}>About me</div>
+              <div>{accountData?.aboutMe}</div>
+            </div>
+          )}
+
+          {accountData?.status && (
+            <div className={styles.wrapper_item}>
+              <div className={styles.title}>Status</div>
+              <div>{accountData?.status}</div>
+            </div>
+          )}
+        </>
+      ) : ( */}
+      <div className={styles.default_content}>
+        <ProfileDataIllustration height="50%" width="50%" mainColor={heyFriendStyleConstant.first} minorColor={heyFriendStyleConstant.second} />
+      </div>
+      {/* // )} */}
+    </div>
+  );
 };
 
 export default Contacts;
